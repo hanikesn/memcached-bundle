139bd0192cc827205a89cf291e059b3c4c880913
Steffen Hanikel <steffen.hanikel@meinfernbus.de>
2015-09-25 13:29:17 +0200

    Fixed crash when parameters are not found

--- DependencyInjection/Compiler/OverrideDoctrineMemcachedCache.php
+++ DependencyInjection/Compiler/OverrideDoctrineMemcachedCache.php
@@ -14,15 +14,18 @@ class OverrideDoctrineMemcachedCache implements CompilerPassInterface
      */
     public function process(ContainerBuilder $container)
     {
-        if($container->getParameter('doctrine_cache.memcached.class') === 'Doctrine\Common\Cache\MemcachedCache') {
+        if($container->has('doctrine_cache.memcached.class') &&
+            $container->getParameter('doctrine_cache.memcached.class') === 'Doctrine\Common\Cache\MemcachedCache') {
             $container->setParameter('doctrine_cache.memcached.class', 'Lsw\MemcacheBundle\Doctrine\Cache\MemcachedCache');
         }
 
-        if($container->getParameter('doctrine.orm.cache.memcached.class') === 'Doctrine\Common\Cache\MemcachedCache') {
+        if($container->hasParameter('doctrine.orm.cache.memcached.class') &&
+            $container->getParameter('doctrine.orm.cache.memcached.class') === 'Doctrine\Common\Cache\MemcachedCache') {
             $container->setParameter('doctrine.orm.cache.memcached.class', 'Lsw\MemcacheBundle\Doctrine\Cache\MemcachedCache');
         }
 
-        if($container->getParameter('liip_doctrine_cache.memcached.class') === 'Doctrine\Common\Cache\MemcachedCache') {
+        if($container->hasParameter('liip_doctrine_cache.memcached.class') &&
+            $container->getParameter('liip_doctrine_cache.memcached.class') === 'Doctrine\Common\Cache\MemcachedCache') {
             $container->setParameter('liip_doctrine_cache.memcached.class', 'Lsw\MemcacheBundle\Doctrine\Cache\MemcachedCache');
         }
     }
